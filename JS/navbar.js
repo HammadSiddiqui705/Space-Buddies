be245af4cf99fd865272ada3e9592c7bef959fb8
Hammad Siddiqui <hammadsiddiqui914@gmail.com>
2025-12-24 23:54:02 +0500

    Update navbar.js

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/Components/navbar.js")
+  fetch("../Components/navbar.js")
     .then((res) => res.text())
     .then((data) => {
       document.getElementById('navbar-placeholder').innerHTML = data;
