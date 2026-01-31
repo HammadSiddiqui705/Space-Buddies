66be2459c33c3e005cefdf379b1f29e6d7de836a
Hammad Siddiqui <hammadsiddiqui914@gmail.com>
2026-01-31 22:56:44 +0500

    .

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,7 @@ let currentIndex = 0;
 let swiper;
 
 // JSON load karna
-fetch("Data/Planets.Json")
+fetch("./Planets.json")
   .then(response => response.json())
   .then(data => {
     planets = data;
